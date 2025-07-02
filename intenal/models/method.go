db92466727f79dce867a2ea747872e2e818143a8
dwincahya <dwincahya8@gmail.com>
2025-07-02 16:44:05 +0700

    update model

--- intenal/models/method.go
+++ intenal/models/method.go
@@ -11,9 +11,9 @@ type PaymentMethodResponse struct {
 	UpdatedAt  int64  `json:"updated_at"`
 }
 type CreatePaymentMethodRequest struct {
-	ID       uint   `json:"id" validate:"required"`
-	Name     string `json:"name" validate:"required,max=255"`
-	Desc     string `json:"desc" validate:"required,max=255"`
-	OrderNum int64  `json:"order_num" validate:"required"`
-	User
+	ID         uint   `json:"id" validate:"required"`
+	Name       string `json:"name" validate:"required,max=255"`
+	Desc       string `json:"desc" validate:"required,max=255"`
+	OrderNum   int64  `json:"order_num" validate:"required"`
+	UserAction string `json:"user_action" validate:"required, max=255"`
 }
