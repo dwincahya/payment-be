1c2e67095a1765f2452a6eae4e0cc0a8889ffc7a
dwincahya <dwincahya8@gmail.com>
2025-07-03 16:48:36 +0700

    converter method

--- intenal/model/converter/method_converter.go
+++ intenal/model/converter/method_converter.go
@@ -1,1 +1,17 @@
 package converter
+
+import (
+	"github.com/dwincahya/payment-be/intenal/entity"
+	models "github.com/dwincahya/payment-be/intenal/model"
+)
+
+func PaymentMethodtoResponse(PaymentMethod *entity.PaymentMethod) *models.PaymentMethodResponse {
+	return &models.PaymentMethodResponse{
+		ID:         PaymentMethod.ID,
+		Name:       PaymentMethod.Name,
+		Desc:       PaymentMethod.Desc,
+		Code:       PaymentMethod.Code,
+		OrderNum:   PaymentMethod.OrderNum,
+		UserAction: PaymentMethod.UserAction,
+	}
+}
